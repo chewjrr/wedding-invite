640cfc7c8fb99cccfb51cf5d4b91bfed2631328f
Всеволод Миронов <chewjr@vk.com>
2025-09-13 12:51:03 +0300

    main.go cors fix

--- backend/main.go
+++ backend/main.go
@@ -56,10 +56,10 @@ func getPort() string {
 // withCORS добавляет заголовки CORS
 func withCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Разрешаем все источники (для простоты). В продакшене можно указать конкретный домен.
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://wedding-frontend-zt57.onrender.com")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		// Для preflight-запросов
 		if r.Method == "OPTIONS" {
